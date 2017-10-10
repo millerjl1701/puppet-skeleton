0bf8325bd551a15a3c6648282e0698f42419bea1
Jeffrey Miller <millerjl1701@gmail.com>
2017-10-10 14:09:08 -0700

    changes made based on pdk module structure, new tasks framework, and vox pupuli testing

--- skeleton/spec/spec_helper.rb
+++ skeleton/spec/spec_helper.rb
@@ -15,6 +15,13 @@ end
 
 if Puppet::Util::Package.versioncmp(Puppet.version, '4.5.0') >= 0
   RSpec.configure do |c|
+    default_facts = {
+      puppetversion: Puppet.version,
+      facterversion: Facter.version,
+    }
+    default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
+    default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
+    c.default_facts = default_facts
     c.before :each do
       Puppet.settings[:strict] = :error
     end
